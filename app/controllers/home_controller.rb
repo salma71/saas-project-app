ef380271c96097cf4be16ae44350566b939cb08c
Salma Elshahawy <salmaeng71@gmail.com>
2019-04-04 12:25:35 -0400

    fix the application helper bug, remove dev-bostp

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,20 @@ class HomeController < ApplicationController
 
   def index
     if current_user
+
       if session[:tenant_id]
         Tenant.set_current_tenant session[:tenant_id]
+
       else
+
         Tenant.set_current_tenant current_user.tenants.first
+
+      end
+
+  @tenant = Tenant.current_tenant
+
+  params[:tenant_id] = @tenant.id
+
     end
-    @tenant = Tenant.current_tenant
-    params[:tenant_id] = @tenant.id
   end
 end
